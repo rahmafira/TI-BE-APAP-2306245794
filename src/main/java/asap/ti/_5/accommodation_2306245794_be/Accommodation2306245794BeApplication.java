5ce3c9fff24e9e9d3487c524dfbb5e86a30ecdcf
Fira <rahmadwimaghfira@Firas-MacBook-Air.local>
2025-11-04 19:41:05 +0700

    done ketentuan proyek

---
+++ src/main/java/asap/ti/_5/accommodation_2306245794_be/Accommodation2306245794BeApplication.java
@@ -0,0 +1,13 @@
+package asap.ti._5.accommodation_2306245794_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Accommodation2306245794BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Accommodation2306245794BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/asap/ti/_5/accommodation_2306245794_be/Accommodation2306245794BeApplicationTests.java
@@ -0,0 +1,13 @@
+package asap.ti._5.accommodation_2306245794_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Accommodation2306245794BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
