3b7eff4076c063cc896fed18730b91e7a2649ba2
Rahma Dwi Maghfira <rahma.dwi@ui.ac.id>
2025-11-23 14:52:02 +0700

    add cors

--- src/main/java/apap/ti/_5/accommodation_2306245794_be/config/WebConfig.java
+++ src/main/java/apap/ti/_5/accommodation_2306245794_be/config/WebConfig.java
@@ -1,11 +1,15 @@
 package apap.ti._5.accommodation_2306245794_be.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${CORS_ALLOWED_ORIGINS}")
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
